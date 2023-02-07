1d38e8e35c8ac9ab765696d331aec0f095cca8c5
a.spichakou <a.spichakou@gmail.com>
2023-02-07 18:00:15 +0100

    Updated transfromation script

--- script/process_v2.sh
+++ script/process_v2.sh
@@ -1,17 +1,20 @@
 #!/bin/bash	
-for item in /media/usb1/fb2.Flibusta.Net/*.zip
+archivepath=/media/usb1/fb2.Flibusta.Net
+outputpath=/media/usb1/work/test/xsltoutput1
+
+for item in $archivepath/*.zip
 do
   echo $(basename ${item})
-  mkdir /media/usb1/work/test/xsltoutput/$(basename ${item})
+  mkdir $outputpath/$(basename ${item})
   for zipfile in `zipinfo -1 ${item}`
   do    
-	if [ -f "/media/usb1/work/test/xsltoutput/$(basename ${item})/${zipfile}.xml" ]; then
-		echo "/media/usb1/work/test/xsltoutput/$(basename ${item})/${zipfile}.xml exists."
+	if [ -f "$outputpath/$(basename ${item})/${zipfile}.xml" ]; then
+		echo "$outputpath/$(basename ${item})/${zipfile}.xml exists."
 	else 
 		echo ${item}/${zipfile}
-		unzip -p ${item} ${zipfile} > /media/usb1/work/test/tmp/${zipfile}
-		xsltproc --stringparam archivename $(basename ${item}) --stringparam filename ${zipfile} ./xslt/transform.xslt /media/usb1/work/test/tmp/${zipfile} > /media/usb1/work/test/xsltoutput/$(basename ${item})/${zipfile}.xml
-		rm /media/usb1/work/test/tmp/${zipfile}
+		unzip -p ${item} ${zipfile} > /tmp/${zipfile}
+		xsltproc --stringparam archivename $(basename ${item}) --stringparam filename ${zipfile} ./xslt/transform.xslt /tmp/${zipfile} > $outputpath/$(basename ${item})/${zipfile}.xml
+		rm /tmp/${zipfile}
 	fi
   done
 done
