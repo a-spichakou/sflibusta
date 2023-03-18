2e19079275a4b7f5cccd2cb01ffe8605c92796ed
a.spichakou <a.spichakou@gmail.com>
2023-03-18 19:45:41 +0100

    Small changes

--- script/transform.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash	
-mkdir /media/usb1/work/test/output/$1
-for item in ./mount/*.fb2
-do
-  echo /media/usb1/work/test/output/$1/$(basename ${item}).xml
-  xsltproc --stringparam archivename f.fb2-183066-183652.zip --stringparam filename $(basename ${item}) ./xslt/transform.xslt  $item > /media/usb1/work/test/output/$1/$(basename ${item}).xml
-done
